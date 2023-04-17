fa817ac6f7734c6dc2eaf2ec801115b69b94a05e
KLINKDOG <wbfox0@gmail.com>
2023-04-17 22:13:07 +0300

    First commit
    Попробовал сделать snap to grid
    Дополнил карту

--- Assets/Scripts/ChangeDirection/ChangeDirectionLEFT.cs
+++ Assets/Scripts/ChangeDirection/ChangeDirectionLEFT.cs
@@ -2,28 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeDirectionLEFT : MonoBehaviour
-{
-    public float speed = 20f;
+//public class ChangeDirectionLEFT : MonoBehaviour
+//{
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            // Получаем направление движения игрока
-            Vector3 playerDirection = collision.gameObject.GetComponent<CharacterMovement>().MoveDir;
+//    public float speed = 20f;
 
-            // Изменяем направление движения на право
-            Vector3 newDirection = Vector3.Cross(Vector3.up, playerDirection);
+//    private void OnCollisionEnter(Collision collision)
+//    {
+//        if (collision.gameObject.CompareTag("Player"))
+//        {
+//            // Получаем направление движения игрока
+//            Vector3 playerDirection = collision.gameObject.GetComponent<CharacterMovement>().MoveDir;
 
-            // Обновляем направление движения игрока
-            collision.gameObject.GetComponent<CharacterMovement>().MoveDir = newDirection.normalized;
+//            // Изменяем направление движения на право
+//            Vector3 newDirection = Vector3.Cross(Vector3.up, playerDirection);
 
-            // Двигаем объект в новом направлении
-            GetComponent<Rigidbody>().velocity = newDirection.normalized * speed;
-        }
-    }
-}
+//            // Обновляем направление движения игрока
+//            collision.gameObject.GetComponent<CharacterMovement>().MoveDir = newDirection.normalized;
+
+//            // Двигаем объект в новом направлении
+//            GetComponent<Rigidbody>().velocity = newDirection.normalized * speed;
+//        }
+//    }
+//}
 
 
 
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
-{
+{ /*
+    
     GameObject character;
-/*    private Vector3 currentPos = Transform.;*/
+    //    private Vector3 currentPos = Transform.;
     public float speed = 5f;
     public LayerMask obstacleLayer;
     public float raycastLength = 1f;
+    public bool isInCharge = false;
 
     private Rigidbody rb;
     private Vector3 moveDir = Vector3.zero;
+    private bool isMoving = false;
 
     public Vector3 MoveDir
     {
         get { return moveDir; }
         set { moveDir = value; }
     }
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,20 +34,23 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && !isMoving)
         {
             moveDir = Vector3.forward;
         }
-        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+
+        if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) && !isMoving)
         {
             moveDir = Vector3.back;
-/*            transform.position = Vector3.(Mathf.Round())*/
+            //           transform.position = Vector3.(Mathf.Round())
         }
-        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+
+        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && !isMoving)
         {
             moveDir = Vector3.left;
         }
-        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+
+        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && !isMoving)
         {
             moveDir = Vector3.right;
         }
@@ -58,12 +65,90 @@ public class CharacterMovement : MonoBehaviour
                 // Если есть препятствие, останавливаем движение и обнуляем направление
                 moveDir = Vector3.zero;
             }
+
             else
             {
                 // Если препятствия нет, двигаем персонажа
                 rb.MovePosition(rb.position + moveDir * speed * Time.deltaTime);
             }
         }
-    }
-}
+    }*/
 
+    
+
+    private bool isMoving;
+    private Vector3 origPos, targetPos;
+    private float timeToMove = 0.2f;
+
+    private void Start()
+    {
+        isMoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.forward));
+        }
+
+        if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.back));
+
+        }
+
+        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.left));
+        }
+
+        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.right));
+        }
+
+        //if (moveDir != Vector3.zero)
+        //{
+        //    Ray moveRay = new Ray(transform.position, moveDir);
+
+
+        //    if (Physics.Raycast(moveRay, out RaycastHit hit, raycastLength, obstacleLayer))
+        //    {
+
+        //        moveDir = Vector3.zero;
+        //    }
+        //    else
+        //    {
+
+        //        rb.MovePosition(rb.position + moveDir * speed * Time.deltaTime);
+        //    }
+        //}
+    }
+
+
+
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        isMoving = true;
+
+        float elapsedTime = 0;
+
+        origPos = transform.position;
+        targetPos = origPos + direction;
+
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+
+}
