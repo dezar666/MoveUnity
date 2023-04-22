a199926427f06c8d883bc74f1be41d962c04ff83
Dreamurr <zekardium@gmail.com>
2023-04-22 06:17:51 +0530

    Make cube lose charge when stopped

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -100,6 +100,7 @@ public class CharacterMovement : MonoBehaviour
 
             if (bShouldYield)
             {
+                isCharged = false;
                 yield break;
             }
         }
