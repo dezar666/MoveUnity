dbdb0c5995fa29c7700027e82968f067132692b1
Dinis Dinislamov <forwator221@gmail.com>
2023-06-18 14:54:31 +0500

    add logging and fps showing

---
+++ Assets/Scripts/Logging/Logger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Logger : MonoBehaviour
+{
+    private string _workDirectory;
+    private FileWriter _fileWriter;
+
+    private void Awake()
+    {
+        _workDirectory = $"{Application.persistentDataPath}/Logs";
+        if (!Directory.Exists(_workDirectory))
+        {
+            Directory.CreateDirectory(_workDirectory);
+        }
+        _fileWriter = new FileWriter(_workDirectory);
+        Application.logMessageReceivedThreaded += OnLogMessageReceived;
+    }
+
+    private void OnLogMessageReceived(string condition, string stacktrace, LogType type)
+    {
+        if (type == LogType.Exception)
+        {
+            _fileWriter.Write(new LogMessage(type, condition));
+            _fileWriter.Write(new LogMessage(type, stacktrace));
+        }
+        else
+        {
+            _fileWriter.Write(new LogMessage(type, condition));
+        }
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            UnityEditor.EditorUtility.RevealInFinder(_workDirectory);
+        }
+#endif
+    }
+
+    private void OnDestroy()
+    {
+        _fileWriter.Dispose();
+    }
+}
---
+++ Assets/Scripts/UI/FpsCounter/FpsCounter.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsCounter : MonoBehaviour
+{
+    [SerializeField]
+    private int _frameRange = 60;
+
+    private int[] _fpsBuffer;
+    private int _fpsBufferIndex;
+
+    public int AverageFPS { get; private set; }
+    public int HighestPFS { get; private set; }
+    public int LowersFPS { get; private set; }
+
+    private void Update()
+    {
+        if (_fpsBuffer == null || _frameRange != _fpsBuffer.Length)
+        {
+            InitializeBuffer();
+        }
+
+        UpdateBuffer();
+        CalculateFps();
+    }
+
+    private void InitializeBuffer()
+    {
+        if (_frameRange <= 0)
+        {
+            _frameRange = 1;
+        }
+
+        _fpsBuffer = new int[_frameRange];
+        _fpsBufferIndex = 0;
+    }
+
+    private void UpdateBuffer()
+    {
+        _fpsBuffer[_fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+        if (_fpsBufferIndex >= _frameRange)
+        {
+            _fpsBufferIndex = 0;
+        }
+    }
+
+    private void CalculateFps()
+    {
+        int sum = 0;
+        int lowest = int.MaxValue;
+        int highest = 0;
+        for (int i = 0; i < _frameRange; i++)
+        {
+            int fps = _fpsBuffer[i];
+            sum += fps;
+            if (fps > highest)
+            {
+                highest = fps;
+            }
+
+            if (fps < lowest)
+            {
+                lowest = fps;
+            }
+        }
+
+        HighestPFS = highest;
+        LowersFPS = lowest;
+        AverageFPS = sum / _frameRange;
+    }
+}
