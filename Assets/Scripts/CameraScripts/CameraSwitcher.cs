905303a46911186a3cdc7a4696033ad441526fae
Dinis Dinislamov <forwator221@gmail.com>
2023-06-18 15:11:43 +0500

    cameras fix

--- Assets/Scripts/CameraScripts/CameraSwitcher.cs
+++ Assets/Scripts/CameraScripts/CameraSwitcher.cs
@@ -31,13 +31,13 @@ public static class CameraSwitcher
     public static void Register(CinemachineVirtualCamera camera)
     {
         cameras.Add(camera);
-        Debug.Log("Camera registered: " + camera);
+        //Debug.Log("Camera registered: " + camera);
     }
 
     public static void Unregister(CinemachineVirtualCamera camera)
     {
         cameras.Remove(camera);
-        Debug.Log("Camera unregistered: " + camera);
+        //Debug.Log("Camera unregistered: " + camera);
     }
 
 }
