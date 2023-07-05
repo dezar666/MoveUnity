7751db8b861d8e4afea6e617b26f732b2f8b8e64
Dinis Dinislamov <forwator221@gmail.com>
2023-07-05 22:07:37 +0500

    add pick up message animation

--- Assets/PickUpMessage.cs
+++ Assets/PickUpMessage.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
+using System;
 
 public class PickUpMessage : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _itemName;
     [SerializeField] private Image _itemIcon;
+
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private RectTransform rectTransform;
+
+    [SerializeField] private float fadeTime;
+    [SerializeField] private float showingTime;
 
     public void ShowMessage(string name, Sprite icon, int collectedItems, int totalItems)
     {
@@ -15,12 +23,30 @@ public class PickUpMessage : MonoBehaviour
         _itemIcon.sprite = icon;
         gameObject.SetActive(true);
         FindObjectOfType<PlayerAudioManager>().SoundOnItemPickUp();
-        StartCoroutine(DeactivateMessage());
+        PanelFadeIn();
     }
 
-    IEnumerator DeactivateMessage()
+    public void PanelFadeIn()
     {
-        yield return new WaitForSeconds(2);
-        gameObject.SetActive(false);
+        canvasGroup.alpha = 0f;
+        rectTransform.transform.localPosition = new Vector3(0f, -500f, 0f);
+        rectTransform.DOAnchorPos(new Vector2(0f, 0f), fadeTime, false).SetEase(Ease.OutElastic);
+        canvasGroup.DOFade(1, fadeTime);
+        StartCoroutine(WaitSomeTime());
+    }
+
+    private void PanelFadeOut()
+    {
+        canvasGroup.alpha = 1f;
+        rectTransform.transform.localPosition = new Vector3(0f, 0f, 0f);
+        //rectTransform.DOAnchorPos(new Vector2(0f, -500f), fadeTime, false).SetEase(Ease.InOutQuint);
+        canvasGroup.DOFade(0, fadeTime);
+
+    }
+
+    IEnumerator WaitSomeTime()
+    {
+        yield return new WaitForSeconds(showingTime);
+        PanelFadeOut();
     }
 }
