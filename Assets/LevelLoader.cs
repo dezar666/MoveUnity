167edd4672593428b81921373746af3a0ba3523f
Dinis <forwator221@gmail.com>
2023-10-17 00:32:09 +0500

    fix mainMenuUi

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -61,12 +61,12 @@ public class LevelLoader : MonoBehaviour, IDatePersistence
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 2; i < buttons.Length;i+=2)
-        {
-            var temp = buttons[i];
-            buttons[i] = buttons[i+1];
-            buttons[i+1] = temp;
-        }
+        //for(int i = 2; i < buttons.Length;i+=2)
+        //{
+        //    var temp = buttons[i];
+        //    buttons[i] = buttons[i+1];
+        //    buttons[i+1] = temp;
+        //}
 
         SetValues(); 
     }
